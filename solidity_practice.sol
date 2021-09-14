2d74ecf32e7eb38bad930c3c8ef46055b6ae5857
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-09-14 11:34:16 -0400

    first contract

--- solidity_practice.sol
+++ solidity_practice.sol
@@ -1,19 +1,35 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.0 <0.9.0;
 
 contract simpleStorage {
     
     // this will get initialized to zero
     uint256 favoriteNumber;
+    bool favoriteBool;
+
+    
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
     
     function store(uint256 _favoriteNumber)public {
         favoriteNumber = _favoriteNumber;
-        uint256 test = 4;
     }
     
     
     // view, pure
     function retrieve() public view returns(uint256){
         return favoriteNumber;
+    }
+    
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
     
 }
